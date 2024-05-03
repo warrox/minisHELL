2ff470f781baf4933ab195212d9dd7290142bcc9
Warren Hamdi <whamdi@bess-f2r1s4.clusters.42paris.fr>
2024-05-03 16:19:38 +0200

    created first parsing func init

--- src/minishell_lib.h
+++ src/minishell_lib.h
@@ -1,22 +1,26 @@
-#ifdef MINISHELL_LIB_H
-# include "libft.h"
-# include "../Printf/ft_printf.h"
+#ifndef MINISHELL_LIB_H
+#define MINISHELL_LIB_H
+#include "../libft/libft.h"
+#include "../Printf/ft_printf.h"
 
-typedef struct s_list_arg
-{
-	struct s_list_arg *next;
-	struct s_list_arg *prev;
-	char *arg_var;
-	char *arg_val
-	char *opt;
-}t_list_arg;
 typedef struct s_data
 {
-		
+	t_list *arg;
+	char **args;
 } t_data;
 
 
 /*************************** INIT FUNC ***********************/
 
 /*************************** PARSING FUNC ***************/
+
+/*******************************************************************/
+/*                        LIST CUSTOM FUNC                     		*/
+/*******************************************************************/
+void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_list_lenght(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+//void				ft_lstclear(t_list **lst);
+void parser(char **arg, t_data *data);
+
 #endif
