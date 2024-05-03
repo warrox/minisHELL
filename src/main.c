d469cd24c3d88c40bfede32438548bb4afadedce
Cyprien Ferreira <cyferrei@bess-f2r2s3.clusters.42paris.fr>
2024-05-03 16:18:54 +0200

    [MODIF]: add 42 header

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
-#include "minishell_lib.h"
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 16:15:30 by cyferrei          #+#    #+#             */
+/*   Updated: 2024/05/03 16:15:32 by cyferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell_lib.h"
+
 int main(int argc, char **argv, char **envp)
 {
 	(void) argc;
