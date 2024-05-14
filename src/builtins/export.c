49c25243e3e96885a3a682462a62bd8961ecbbc8
Cyprien Ferreira <cyferrei@paul-f4Ar6s7.clusters.42paris.fr>
2024-05-14 19:35:16 +0200

    [MODIF]: export builtins now handle with and without ARGS - change value if ok - tests OK - NO leaks

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,17 +6,60 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 15:46:42 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/05/14 11:54:46 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/05/14 19:03:00 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell_lib.h"
 
-//export sans ARGS
+//export sans ARGS OK
 //export avec ARGS
 //parsing des ARGS
 //export +=
 //env -i export
+
+static void	set_value(t_data *data, char *value, char *new)
+{
+	t_list_arg *tmp;
+
+	tmp = data->lst;
+	while (tmp && ft_strcmp(tmp->key_and_val[0], value) != 0)
+		tmp = tmp->next;
+	if (tmp->next == NULL)
+		return;
+	else
+	{
+		free(tmp->key_and_val[1]);
+		tmp->key_and_val[1] = ft_strdup(new);
+	}
+}
+
+static int check_egals(char *input)
+{
+	int i;
+
+	i = ZERO_INIT;
+	while(input[i])
+	{
+		if(input[i] == '=')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static void free_split(char **split)
+{
+	int i;
+	
+	i = ZERO_INIT;
+	while(split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
 
 static void	print_lst_export(t_list_arg *lst)
 {
@@ -29,11 +72,44 @@ static void	print_lst_export(t_list_arg *lst)
 	}
 }
 
-void	cmd_export(t_data *data, char *input)
+void cmd_export(t_data *data, char *input) 
 {
-	int i; 
-	i = ZERO_INIT;
-	
-	if(ft_strncmp(input, "export", 7) == 0 && data->lst != NULL)
-		print_lst_export(data->lst);
+	char **split = NULL;
+   	char **arg = NULL;
+    t_list_arg *tmp = data->lst;
+	int i = 1;
+	int j = 1;
+
+    if (ft_strcmp(input, "export") == 0 && data->lst != NULL) 
+        print_lst_export(data->lst);
+	else
+	{
+		split = ft_split(input, ' ');
+		if (!split)
+			return;
+		while(split[i])
+		{
+			arg = ft_split(split[i], '=');
+			if(!arg)
+			{
+				free_split(split);
+				return;
+			}
+			tmp = data->lst;
+			while(tmp)
+			{
+				if(ft_strcmp(arg[0], tmp->key_and_val[0]) == 0 && check_egals(split[j])) 
+				{
+					set_value(data, tmp->key_and_val[0], arg[1]);
+					//printf("MATCH\n");
+					//break;
+				}
+				tmp = tmp->next;
+			}
+			free_split(arg);
+			j++;
+			i++;
+		}
+		free_split(split);
+	}
 }
