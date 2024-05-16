e44765d5bf0c209ab7c36ca76785b19bbe284eec
Cyprien Ferreira <cyferrei@bess-f1r5s11.clusters.42paris.fr>
2024-05-16 16:46:45 +0200

    [MODIF]: add to export builtins new env var it doesnt exist yet - TESTS OK - LEAKS (with parsing parts)

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 15:46:42 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/05/14 19:03:00 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/05/16 16:03:56 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static void	set_value(t_data *data, char *value, char *new)
 	tmp = data->lst;
 	while (tmp && ft_strcmp(tmp->key_and_val[0], value) != 0)
 		tmp = tmp->next;
-	if (tmp->next == NULL)
+	if (tmp == NULL)
 		return;
 	else
 	{
@@ -80,6 +80,8 @@ void cmd_export(t_data *data, char *input)
 	int i = 1;
 	int j = 1;
 
+	if(ft_strcmp(input, "") == 0)
+		return;
     if (ft_strcmp(input, "export") == 0 && data->lst != NULL) 
         print_lst_export(data->lst);
 	else
@@ -96,19 +98,27 @@ void cmd_export(t_data *data, char *input)
 				return;
 			}
 			tmp = data->lst;
+			int count = 0;
 			while(tmp)
 			{
 				if(ft_strcmp(arg[0], tmp->key_and_val[0]) == 0 && check_egals(split[j])) 
 				{
 					set_value(data, tmp->key_and_val[0], arg[1]);
-					//printf("MATCH\n");
-					//break;
+					printf("MATCH\n");
+					break;
 				}
 				tmp = tmp->next;
+				count++;
+			}
+			if (tmp == NULL)
+			{
+				ft_printf("%s\n", split[i]);
+				ft_lstadd_arg_back(&data->lst, ft_lst_arg_new(data->lst, split[i]));
 			}
 			free_split(arg);
 			j++;
 			i++;
+			
 		}
 		free_split(split);
 	}
