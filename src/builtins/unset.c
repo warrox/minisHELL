499ab9165d323afc5d3d01811a0dd3226eeb432d
Cyprien Ferreira <cyferrei@paul-f4Ar5s6.clusters.42paris.fr>
2024-05-17 16:29:56 +0200

    [MODIF]: builtin can recognize the value to unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 19:46:41 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/05/16 20:38:03 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/05/17 16:04:24 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,22 @@ void	cmd_unset(t_data *data, char *input)
 	int i = 0;
 	t_list_arg *tmp = data->lst;
 	
-	if(ft_strcmp(input, "unset") == 0 && data->lst != NULL)
+	if(ft_strncmp(input, "unset", 5) == 0 && data->lst != NULL)
 	{
 		split = ft_split(input, ' ');
 		if (!split)
-			dprintf(2, "tests\n");
-			//return;
-		if (split[i + 1] == NULL)
 			return;
+		if (split[i + 1] == NULL)
+			return(free_split(split));
 		while(tmp)
 		{
-			printf("%s\n", split[i]);
+			printf("%s\n", split[i + 1]);
 			printf("%s\n", tmp->key_and_val[0]);
-			if(ft_strcmp(split[i], tmp->key_and_val[0]))
-			{
+			if(!ft_strcmp(split[i + 1], tmp->key_and_val[0]))
+			{ 
 				printf("OK\n");
 				free_split(split);
-				break;
+				return;
 			}
 			tmp = tmp->next;
 		}
