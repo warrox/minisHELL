5140670f3e61c297dd4410e95f8c87b9edf66e23
Cyprien Ferreira <cyferrei@paul-f4Ar5s6.clusters.42paris.fr>
2024-05-17 16:27:36 +0200

    [ADD]: ft_strstr.c

---
+++ libft/ft_strstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 14:39:58 by cyferrei          #+#    #+#             */
+/*   Updated: 2024/05/17 15:55:12 by cyferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+#include <stdio.h>
+
+int	ft_strstr(char *str, char *occ)
+{
+	int i;
+	int j;
+	int k;
+	k = 0;
+	
+	if (!str || !occ)
+		return(0);
+	while(str[k])
+	{
+		i = k;
+		j = 0;
+		while(str[i] == occ[j])
+		{
+			i++;
+			j++;
+		}
+		if(!occ[j])
+			return(1);
+		k++;
+	}
+	return(0);	
+}
