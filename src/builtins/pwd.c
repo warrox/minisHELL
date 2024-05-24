3376c13f24a93461e9a72242566c557a90871f72
Cyprien Ferreira <cyferrei@bess-f2r4s16.clusters.42paris.fr>
2024-05-24 16:24:55 +0200

    [MODIF]: pwd now works with tokenizer... YES

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,18 +6,18 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 14:54:02 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/05/17 17:31:10 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/05/24 16:16:57 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell_lib.h"
 
-void	pwd_cmd(char *input)
+void	pwd_cmd(t_data *data)
 {
 	char	*value;
 
 	value = NULL_INIT;
-	if (ft_strncmp(input, "pwd", 3) == 0)
+	if (ft_strncmp(data->tokenizer->cmd_and_arg[0], "pwd", 3) == 0)
 	{
 		value = getcwd(NULL, 0);
 		ft_printf("%s\n", value);
