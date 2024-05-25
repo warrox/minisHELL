9f9edef5a07c67eaf0c5831874ee2385a540d119
Cyprien Ferreira <cyferrei@bess-f1r6s3.clusters.42paris.fr>
2024-05-25 18:09:41 +0200

    [ADD]: add new file for builtins manager

---
+++ src/builtins/builtins_utils.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 16:28:47 by cyferrei          #+#    #+#             */
+/*   Updated: 2024/05/25 16:45:28 by cyferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell_lib.h"
+
+void	is_a_builtin(t_data *data)
+{
+	cmd_env(data);
+	pwd_cmd(data);
+	cmd_export(data);
+	cmd_unset(data);
+	cmd_echo(data);
+}
