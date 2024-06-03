3d8393c7ae464032d23d0cb50989bd23b6f097cd
PierreLgol <plgol.perso@gmail.com>
2024-06-03 18:41:23 +0200

    [ADD]: add some utils functions in this folder

---
+++ src/utils/print_utils.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 01:56:57 by cyferrei          #+#    #+#             */
+/*   Updated: 2024/06/02 21:05:56 by cyferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell_lib.h"
+
+void    print_prompt_struct(t_data *data)
+{
+    t_prompt *tmp;
+    
+    tmp = data->prompt;
+    printf("----------------------------------------\n");
+    printf("            PROMPT STRUCTURE            \n");
+    printf("----------------------------------------\n");
+    printf("[usr_name] = %s\n", tmp->usr_name);
+    printf("[usr_pwd] = %s\n", tmp->usr_pwd);
+    printf("[usr_build_one] = %s\n", tmp->usr_build_one);
+    printf("[usr_build_two] = %s\n", tmp->usr_build_two);
+    printf("[usr_prompt] = %s\n", tmp->usr_prompt);
+    printf("----------------------------------------\n");
+}
