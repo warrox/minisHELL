9c3adf277d2db20154e943aafcfe887d38c9a2ed
PierreLgol <plgol.perso@gmail.com>
2024-06-03 18:42:46 +0200

    [MODIF]: now works whith final_cmd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 14:54:02 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/05/30 14:29:20 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/06/02 21:20:56 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	pwd_cmd(t_data *data)
 {
 	char	*value;
-
+	
 	value = NULL_INIT;
 	if (ft_strncmp(data->tokenizer->final_cmd, "pwd", 3) == 0)
 	{
