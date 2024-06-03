4bb2865905b69a329b4f17763b64659eb2ffee54
PierreLgol <plgol.perso@gmail.com>
2024-06-03 21:33:06 +0200

    [ADD]: add new function to print prompt stuct value

--- src/builtins/builtins_utils.c
+++ src/builtins/builtins_utils.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 16:28:47 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/05/30 14:26:53 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/06/01 15:00:45 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	is_a_builtin(t_data *data)
 {
-	ft_printf("CMD ARG 0 : %s\n",data->tokenizer->final_cmd);
+	//print_lst_cmdarg(data->lst);
 	cmd_env(data);
 	pwd_cmd(data);
 	cmd_export(data);
