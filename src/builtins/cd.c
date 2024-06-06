4a52ea2f1bbdbe28f3ba4c2ff9fe1316d6249c31
Warren Hamdi <whamdi@bess-f2r4s14.clusters.42paris.fr>
2024-06-06 15:20:22 +0200

    [BUILTIN/CD : Progress

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,14 +1,34 @@
 #include "../../includes/minishell_lib.h"
 #include <unistd.h>
 
+
+t_list_arg *find_key_user(t_data *data)
+{
+	t_list_arg *tmp;
+	tmp = data->lst;
+	while(tmp)
+	{
+		if(ft_strcmp(tmp->key_and_val[0], "USER") == 0)
+		{
+			ft_printf("yox : %s\n",tmp->key_and_val[0]);
+			break;
+		}
+		tmp = tmp->next;
+	}
+	return(tmp);
+} 
+
 t_list_arg *find_key_old_pwd(t_data *data)
 {
 	t_list_arg *tmp;
 	tmp = data->lst;
 	while(tmp)
 	{
-		if(ft_strcmp(data->lst->key_and_val[1], "OLDPWD"))
+		if(ft_strcmp(tmp->key_and_val[0], "OLDPWD") == 0)
+		{
+			ft_printf("yox : %s\n",tmp->key_and_val[0]);
 			break;
+		}
 		tmp = tmp->next;
 	}
 	return(tmp);
@@ -20,14 +40,17 @@ t_list_arg *find_key_pwd(t_data *data)
 	tmp = data->lst;
 	while(tmp)
 	{
-		if(ft_strcmp(data->lst->key_and_val[1], "PWD"))
+		if(ft_strcmp(tmp->key_and_val[0], "PWD")== 0)
+		{
+			ft_printf("yo : %s\n",tmp->key_and_val[0]);
 			break;
+		}
 		tmp = tmp->next;
 	}
 	return(tmp);
 } 
 
-void cd_check_opt(char *path, t_data *data)
+int cd_check_opt(char *path, t_data *data)
 {
 	//check if '-'is found if true replace pwd by old path and so on.
 	int i;
@@ -36,39 +59,36 @@ void cd_check_opt(char *path, t_data *data)
 	t_list_arg *save_current;
 	int buffer_size = 4096;
 	char buffer[buffer_size];
-	i = 2;
-	ft_printf("XGOIN\n");
-	ft_printf("ici : %s\n",path);
-	while(path[i++])
+	// ft_printf("ici : %s\n",path);
+	while(path[i] == ' ' || path[i] == '\t')
+		i++;
+	if(path[i]== '-')
 	{
-		ft_printf("YGOIN\n");
-		if(path[i] == ' ' || path[i] == '\t')
+		ft_printf("PEDQUER1\n");
+		while(path[i]== ' ' || path[i] == '\t')
+			i++;
+		if(path[i + 1] == '\0')
 		{
-			while(path[i] == ' ' || path[i] == '\t')
-				i++;
-			if(path[i]== '-')
-			{
-				ft_printf("XGOIN\n");
-				i++;
-				while(path[i] == ' ' || path[i] == '\t')
-					i++;
-				if(path[i + 1] != '\0')
-					break;
-				else
-				{
-					tmp = find_key_old_pwd(data);
-					save_current = find_key_pwd(data);
-					chdir(tmp->key_and_val[1]);
-					free(tmp->key_and_val[1]);
-					tmp->key_and_val[1] = ft_strdup(save_current->key_and_val[1]);
-					free(save_current->key_and_val[1]);
-					getcwd(buffer, buffer_size);
-					save_current->key_and_val[1] = ft_strdup(buffer);	
-					// replace old by new & so on
-				}
-			}
+			ft_printf("PEDQUER\n");
+			tmp = find_key_old_pwd(data);
+			save_current = find_key_pwd(data);
+			chdir(tmp->key_and_val[1]);
+			free(tmp->key_and_val[1]);
+			tmp->key_and_val[1] = ft_strdup(save_current->key_and_val[1]);
+			free(save_current->key_and_val[1]);
+			getcwd(buffer, buffer_size);
+			save_current->key_and_val[1] = ft_strdup(buffer);	
+			ft_printf("%s\n",save_current->key_and_val[0]);
+			return(1);
+			// replace old by new & so on
+		}
+		else
+		{
+			ft_printf("cd: string not in pwd: -\n");
+			return(1);
 		}
 	}
+	return (0);
 }
 
 void ft_current_directory(char *path,t_data *data)
@@ -83,16 +103,32 @@ void ft_current_directory(char *path,t_data *data)
 	char buffer[buffer_size];
 	char buffer_old[buffer_size];
 	int i; 
+	char path_hu[4096];
 	tmp = data->lst;
 	i = 0;
 	if (ft_strstr(data->tokenizer->final_cmd, "cd"))
 	{
 		
-		cd_check_opt(path,data);
+		if(data->tokenizer->final_cmd[i + 2] == '\0')
+		{
+			ft_printf("MACDOOM\n");
+			// /home/whamdi
+			tmp = find_key_user(data);
+			ft_printf("MAGLOIRE : %s\n",tmp->key_and_val[1]);
+			ft_printf("JEAN EDOUARD : %d\n",ft_strlen(tmp->key_and_val[1]));
+			ft_strcpy(path_hu, "/home/");
+			ft_strlcat(path_hu, tmp->key_and_val[1], (ft_strlen(path_hu) + ft_strlen(tmp->key_and_val[1])+ 1));
+			ft_printf("LOANA : %s\n",path_hu);
+			chdir(path_hu);
+			return;
+		}
+		if(cd_check_opt(path,data))
+		{
+			return;
+		}
 		if(chdir(path)== -1)
 		{
 			ft_printf("no such file or directory: %s\n",path);
-			exit(1); //TO REMOOOOOVE
 			return;
 		}
 
