cb17af785b219e12c3ccd07b013a5d1059efb926
Warren Hamdi <whamdi@made-f0Br7s2.clusters.42paris.fr>
2024-06-06 10:32:29 +0200

    [BUILTIN/CD]:Adding debug print

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -36,15 +36,19 @@ void cd_check_opt(char *path, t_data *data)
 	t_list_arg *save_current;
 	int buffer_size = 4096;
 	char buffer[buffer_size];
-
+	i = 2;
+	ft_printf("XGOIN\n");
+	ft_printf("ici : %s\n",path);
 	while(path[i++])
 	{
+		ft_printf("YGOIN\n");
 		if(path[i] == ' ' || path[i] == '\t')
 		{
 			while(path[i] == ' ' || path[i] == '\t')
 				i++;
 			if(path[i]== '-')
 			{
+				ft_printf("XGOIN\n");
 				i++;
 				while(path[i] == ' ' || path[i] == '\t')
 					i++;
@@ -72,6 +76,8 @@ void ft_current_directory(char *path,t_data *data)
 	// 1 copier le path absolut via getcwd et remplacer old_pwd
 	// 2 chdir et check si ok 
 	// si ok remplacer old_pwd et pwd
+	// if cd with no arg chdir goin to home root.
+	// cd /home/$user
 	int buffer_size = 4096;
 	t_list_arg *tmp;
 	char buffer[buffer_size];
@@ -82,27 +88,25 @@ void ft_current_directory(char *path,t_data *data)
 	if (ft_strstr(data->tokenizer->final_cmd, "cd"))
 	{
 		
-		ft_printf("GOIN\n");
-		ft_printf("RETURN OF CHDIR : %d\n",chdir(path));
+		cd_check_opt(path,data);
 		if(chdir(path)== -1)
 		{
 			ft_printf("no such file or directory: %s\n",path);
+			exit(1); //TO REMOOOOOVE
 			return;
 		}
 
 		getcwd(buffer_old, buffer_size);
 		if(chdir(path) == 0)
 		{
-			ft_printf("GOIN2\n");
-			cd_check_opt(path,data);
+			ft_printf("RETURN OF CHDIR : %d\n",chdir(path));
 			tmp = find_key_pwd(data);
-			//use getcwd stock dans buffer et dup buffer dans keyandval
 			ft_printf("TMP 1 : %s\n",tmp->key_and_val[1]);
 			free(data->lst->key_and_val[i]);
 			getcwd(buffer, buffer_size);
 			tmp->key_and_val[i] = ft_strdup(buffer);
 			tmp = find_key_old_pwd(data);
-			ft_printf("TMP 1 : %s\n",tmp->key_and_val[1]);
+			ft_printf("TMP 2 : %s\n",tmp->key_and_val[1]);
 			free(data->lst->key_and_val[i]);
 			data->lst->key_and_val[i] = ft_strdup(buffer_old);
 		}
