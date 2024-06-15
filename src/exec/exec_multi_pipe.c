f9822df4a0a2a6bb85610c5bcb86c75b00abdb5c
Cyprien Ferreira <cyferrei@bess-f2r6s5.clusters.42paris.fr>
2024-06-15 18:44:41 +0200

    [ADD]: multi pipes are now handled - some specific case need to checked - need to implement free and exit function

--- src/exec/exec_multi_pipe.c
+++ src/exec/exec_multi_pipe.c
@@ -6,17 +6,24 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 12:27:54 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/06/14 17:23:18 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/06/15 18:38:00 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell_lib.h"
 
+void	reset_in_out(t_data *data)
+{
+	data->exec->infile = 0;
+	data->exec->outfile = 1;
+}
+
 void	children_process(t_data *data)
 {
 	int i;
 	t_list_arg *tmp = data->tokenizer;
-
+	
+	
 	i = ZERO_INIT;
 	data->exec->pid = fork();
 	if (data->exec->pid == -1)
@@ -31,16 +38,44 @@ void	children_process(t_data *data)
 		tmp = tmp->next;
 		i++;
 	}
+	// if (is_redir(tmp))
+	// {
+	// 	while(tmp->array_sign[i] != 0)
+	// 		init_files(data, tmp, i++);
+	// }
 	if (!data->exec->pid)
 	{
 		if (data->exec->index == 0)
+		{
+			//printf("1\n");
+			first_pipe(data, tmp);
+		}
+		else if (data->exec->index == data->exec->nb_node)
+		{
+			//printf("3\n");
+			last_pipe(data, tmp);
+		}
+		else
+		{
+			//printf("2\n");
+			intermediate_pipe(data, tmp);
+		}
+		close_tubes(data);
+		data->exec->cmd = build_cmd(data, tmp);
+		if (!data->exec->cmd)
+			//free function
 			return;
-			//first_pipe(data->tokenizer);
+		execve(data->exec->cmd, tmp->cmd_array, NULL);
+		//error function
 	}
 }
 
 void	exec_multi_pipe(t_data *data)
 {
+	int	i;
+	//int status = 0;
+
+	i = ZERO_INIT;
 	data->exec->nb_node = nb_node(data);
 	data->exec->nb_tube = (data->exec->nb_node - 1) * 2;
 	data->exec->multi_tube = (int *)malloc(sizeof(int) * (data->exec->nb_tube));
@@ -49,6 +84,7 @@ void	exec_multi_pipe(t_data *data)
 	// printf("ICI -> %d\n", data->exec->nb_node);
 	// printf("La -> %d\n", data->exec->nb_tube);
 	init_tubes(data);
+	//dprintf(2, "idx_init %d\n", data->exec->index);
 	while(++(data->exec->index) < data->exec->nb_node)
 	{
 		if(!data->exec->multi_tube)
@@ -56,7 +92,8 @@ void	exec_multi_pipe(t_data *data)
 		children_process(data);
 	}
 	close_tubes(data);
-	while (wait(NULL) > 0)
+	while (wait(NULL) != -1)
 		;
+	//free function
 	free(data->exec->multi_tube);
 }
--- src/exec/exec_multi_pipe_utils.c
+++ src/exec/exec_multi_pipe_utils.c
@@ -6,16 +6,95 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 15:01:39 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/06/14 17:22:47 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/06/15 18:37:31 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell_lib.h"
 
-// void	first_pipe(t_list_arg *tok)
-// {
-	
-// }
+void	intermediate_pipe(t_data *data, t_list_arg *tok)
+{
+	int	i;
+
+	i = 0;
+	(void)tok;
+	reset_in_out(data);
+	if (is_redir(tok))
+	{
+		while(tok->array_sign[i] != 0)
+			init_files(data, tok, i++);
+	}
+	if (data->exec->infile != 0)
+	{
+		dup2(data->exec->infile, STDIN_FILENO);
+		close(data->exec->infile);
+	}
+	else
+		dup2(data->exec->multi_tube[2 * data->exec->index - 2], STDIN_FILENO);
+	if (data->exec->outfile != 1)
+	{
+		dup2(data->exec->outfile, STDOUT_FILENO);
+		close(data->exec->outfile);
+	}
+	else
+	{
+		// dprintf(2, "LA -> %d\n", data->exec->index);
+		// dprintf(2, " ICI %d\n", data->exec->multi_tube[2 * data->exec->index + 1]);
+		dup2(data->exec->multi_tube[2 * data->exec->index + 1], STDOUT_FILENO);
+	}
+	// dprintf(2, "out %d\n", data->exec->outfile);
+}
+
+void	last_pipe(t_data *data, t_list_arg *tok)
+{
+	int	i;
+	(void)tok;
+	i = 0;
+	reset_in_out(data);
+	if (is_redir(tok))
+	{
+		while(tok->array_sign[i] != 0)
+			init_files(data, tok, i++);
+	}
+	if (data->exec->infile != 0)
+	{
+		dup2(data->exec->infile, STDIN_FILENO);
+		close(data->exec->infile);
+	}
+	else
+		dup2(data->exec->multi_tube[data->exec->index * 2 - 2], STDIN_FILENO);
+	if (data->exec->outfile != 1)
+	{
+		dup2(data->exec->outfile, STDOUT_FILENO);
+		close (data->exec->outfile);
+	}
+}
+
+void	first_pipe(t_data *data, t_list_arg *tok)
+{
+	int	i;
+	(void)tok;
+	i = ZERO_INIT;
+	// dprintf(2, "idx_bf %d\n", data->exec->index);
+	reset_in_out(data);
+	if (is_redir(tok))
+	{
+		while(tok->array_sign[i] != 0)
+			init_files(data, tok, i++);
+	}
+	if (data->exec->infile != 0)
+	{
+		dup2(data->exec->infile, STDIN_FILENO);
+		close(data->exec->infile);
+	}
+	if (data->exec->outfile != 1)
+	{
+		dup2(data->exec->outfile, STDOUT_FILENO);
+		close(data->exec->outfile);
+	}
+	else
+		dup2(data->exec->multi_tube[1], STDOUT_FILENO);
+}
 
 void	close_tubes(t_data *data)
 {
