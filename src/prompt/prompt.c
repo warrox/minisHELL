e15cd4b29cb4aba2b3a8554fd752d378b9b89dd9
Cyprien Ferreira <cyferrei@paul-f4Ar5s4.clusters.42paris.fr>
2024-06-18 18:17:52 +0200

    [NOMODIF]: remove/add some print debug

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 19:17:55 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/06/17 16:57:41 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/06/18 17:59:50 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ int	display_prompt(t_data *data)
 		if(int_nbr >= 0 && int_nbr <= 255)
 			return(int_nbr);
 		input_cpy = parser(input, data);
-		//print_exec_utils(data);
+		// print_exec_utils(data);
 		if(is_a_builtin(data))
 		{
 			add_history(input);
