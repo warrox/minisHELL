bbede6fdf5acb4770d2bef88894d999740ea1e21
Warren Hamdi <whamdi@paul-f4Ar4s1.clusters.42paris.fr>
2024-06-23 10:50:26 +0200

    [EXPANDER]: done

--- src/expander.c
+++ src/expander.c
@@ -2,136 +2,108 @@
 #include "stdbool.h"
 #include <stdio.h>
 
-
 bool isExitCode(char *input, int *i)
 {
-	return(!ft_strncmp(&input[*i],"$?", 2));
+    return (!ft_strncmp(&input[*i], "$?", 2));
 }
+
 bool isSingleQuote(char c)
 {
-	return (c == '\'');
+    return (c == '\'');
 }
 
-char *passTilNextQuote(char *input, int *i, char *buffer) 
+char *passTilNextQuote(char *input, int *i, char *buffer, int *j)
 {
-	int j;
-	j = 0;
-	(*i)++;
-	while (input[*i] && !isSingleQuote(input[*i]))
-	{
-		buffer[j] = input[(*i)];
-		(*i)++;
-		j++;
-	}
-	if(input[(*i)] != '\0')
-	{
-		(*i)++;
-	}
-	return(buffer);
-}
-bool isHereDoc(char *input, int*i)
-{
-	return(!ft_strncmp(&input[*i],"<<",2));
-}
-
-void passVarDoc(char *input, int *i, char *buffer)
-{
-	int j;
-	
-	j = 0;
-	while(j != 2)
-		buffer[j++] = input[(*i)++];
-	// (*i)  += 2;
-	while(input[*i] && ft_isws(input[*i]))
-		(*i)++;
-	while(input[*i] && !ft_isws(input[*i]))
-	{
-		buffer[j] = input[(*i)++];
-		(*i)++;
-		j++;
-	}
-}
-
-bool isVariable(char *input,int *i)
-{
-	return(!ft_strncmp(&input[*i], "$", 1));
-}
-
-void expandVariable(t_data *data, char *input, int *i, char *buffer)
-{
-	(*i)++;
-	t_list_arg *current;
-	int j;
-	int buffer_len;
-	size_t key_len;
-
-	key_len = 0;
-	j = 0;
-
-	current = data->lst;
-	buffer_len = ft_strlen(buffer);
-	printf("COMBIEN : %d\n",buffer_len);
-	while (current)
+    (*i)++;  // Pass the opening quote
+    while (input[*i] && !isSingleQuote(input[*i]))
     {
-        key_len = ft_strlen(current->key_and_val[0]);
+        buffer[(*j)++] = input[*i];
+        (*i)++;
+    }
+    if (input[*i] == '\'')
+    {
+        (*i)++;
+    }
+    buffer[*j] = ' ';
+    (*j)++;
+    buffer[*j] = '\0'; 
+    return buffer;
+}
+
+bool isHereDoc(char *input, int *i)
+{
+    return (!ft_strncmp(&input[*i], "<<", 2));
+}
+
+void passVarDoc(char *input, int *i, char *buffer, int *j)
+{
+    (*i) += 2;
+    while (input[*i] && ft_isws(input[*i]))
+    {
+        (*i)++;
+    }
+    while (input[*i] && !ft_isws(input[*i]))
+    {
+        buffer[(*j)++] = input[(*i)++];
+    }
+    buffer[*j] = ' ';
+    (*j)++;
+    buffer[*j] = '\0';
+}
+
+bool isVariable(char *input, int *i)
+{
+    return (!ft_strncmp(&input[*i], "$", 1));
+}
+
+void expandVariable(t_data *data, char *input, int *i, char *buffer, int *j)
+{
+    (*i)++;
+    t_list_arg *current = data->lst;
+    while (current)
+    {
+        size_t key_len = ft_strlen(current->key_and_val[0]);
         if (!ft_strncmp(&input[*i], current->key_and_val[0], key_len))
         {
-            while(j++ < buffer_len)
-			{
-				printf("GOIN\n");
-				(*i)++;
-			}
-			ft_strlcat(buffer, current->key_and_val[1], buffer_len + strlen(current->key_and_val[1]) + 1);
-            buffer_len += strlen(current->key_and_val[1]);
+            ft_strlcat(buffer, current->key_and_val[1], 4096);
+            *j += ft_strlen(current->key_and_val[1]);
+            *i += key_len;
+            break;
         }
         current = current->next;
     }
+    buffer[*j] = ' ';
+    (*j)++;
+    buffer[*j] = '\0';
 }
 
-
-
-void expander(t_data *data, char *input) 
+void expander(t_data *data, char *input)
 {
-	(void) data;
-	int i;
-	int j;
-	char strExpanded[4096];
-	ft_bzero(strExpanded, 4096);
-	i = 0;
-	j = 0;
-	data->tokenizer->final_cmd = NULL;
-	while(input[i]) 
-	{
-		if (isSingleQuote(input[i]))
-		{
-			// ft_strjoin(strExpanded,passTilNextQuote(input, &i));
-			passTilNextQuote(input, &i, strExpanded);
-		}
-		if (isHereDoc(input, &i)) 
-		{
-			passVarDoc(input, &i, strExpanded);
-		} 
-		if (isExitCode(input,&i)) 
-		{
-			printf("ENTER EXIT CODE DO THE FUNC\n");
-			// expandExitCode();
-		} 
-		if (isVariable(input,&i)) 
-		{
-			expandVariable(data,input,&i, strExpanded);
-			printf("BOLOSEE3 : %s\n", strExpanded);
-		}
-		else
-		{
-			while(strExpanded[j] != 0)
-				j++;
-			strExpanded[j] = input[i];
-			i++;
-		}
-	}
-	data->tokenizer->final_cmd = ft_strdup(strExpanded);
-	printf("what : %s\n", data->tokenizer->final_cmd);
-	printf("FINAL : %s\n", strExpanded);
+    int i = 0;
+    int j = 0;
+    char strExpanded[4096];
+    ft_bzero(strExpanded, 4096);
 
+    while (input[i])
+    {
+        if (isSingleQuote(input[i]))
+            passTilNextQuote(input, &i, strExpanded, &j);
+        else if (isHereDoc(input, &i))
+            passVarDoc(input, &i, strExpanded, &j);
+        else if (isExitCode(input, &i))
+        {
+            printf("ENTER EXIT CODE DO THE FUNC\n");
+            // expandExitCode();
+        }
+        else if (isVariable(input, &i))
+            expandVariable(data, input, &i, strExpanded, &j);
+        else
+        {
+    //         if(input[i] == '\"')
+				// i++;
+			strExpanded[j++] = input[i++];
+        }
+    }
+    strExpanded[j] = '\0';
+    data->tokenizer->final_cmd = ft_strdup(strExpanded);
 }
-
