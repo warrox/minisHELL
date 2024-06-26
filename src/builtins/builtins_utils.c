9e9aca4e6071dd85ac03d0e1115b805a963a1c3e
Cyprien Ferreira <cyferrei@made-f0Br8s4.clusters.42paris.fr>
2024-06-26 11:48:34 +0200

    [MODIF]: connecting buitins

--- src/builtins/builtins_utils.c
+++ src/builtins/builtins_utils.c
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 16:28:47 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/06/25 18:06:10 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/06/26 11:31:26 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ void	exec_builtin(t_data *data, int builtin)
 		cmd_env(data);
 	else if (builtin == PWD)
 		pwd_cmd(data);
-	// else if (builtin == EXPORT)
-	// else if (builtin == UNSET)
-	// else if (builtin == ECHO)
-	// else if (builtin == CD)
+	else if (builtin == EXPORT)
+		cmd_export(data);
+	else if (builtin == UNSET)
+		cmd_unset(data);
+	else if (builtin == ECHO)
+		cmd_echo(data);
+	else if (builtin == CD)
+		ft_current_directory(data->tokenizer->cmd_array[1], data);
 }
 
 int	is_a_builtin(t_data *data)
