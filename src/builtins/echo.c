a977cac44b23a6c30eff81d778e6bf1fbff76fa6
Cyprien Ferreira <cyferrei@made-f0Br7s1.clusters.42paris.fr>
2024-06-28 14:17:15 +0200

    [MODIF]: echo with flag now works

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,29 +6,32 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 16:27:12 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/06/27 18:04:50 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/06/28 14:15:53 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell_lib.h"
 
-void	print_echo_flag(t_data *data, int start)
+void print_echo_flag(t_data *data, int start)
 {
-	int i = 1;
-	int flag = 0;
-	while(data->tokenizer->cmd_array[flag + 1])
-		flag++;	
-	while(data->tokenizer->cmd_array[start + 1])
-	{
-		flag--;
-		write(data->exec->outfile, data->tokenizer->cmd_array[start + 1], ft_strlen(data->tokenizer->cmd_array[start + 1]));
-		if (flag > 0)
-		{
-			write(data->exec->outfile, " ", 1);		
-		}
-		i++;
-	}
-	write(data->exec->outfile, "\n", 1);
+    int i = start + 1;
+    int last = 0;
+
+    // Find the index of the last element
+    while (data->tokenizer->cmd_array[last + 1])
+        last++;
+
+    // Print the elements
+    while (data->tokenizer->cmd_array[i])
+    {
+        write(data->exec->outfile, data->tokenizer->cmd_array[i], ft_strlen(data->tokenizer->cmd_array[i]));
+        if (i < last)
+        {
+            write(data->exec->outfile, " ", 1);
+        }
+        i++;
+    }
+    // write(data->exec->outfile, "\n", 1);
 }
 
 void	print_echo(t_data *data)
@@ -52,41 +55,41 @@ void	print_echo(t_data *data)
 	write(data->exec->outfile, "\n", 1);
 }
 
-int	check_flag(t_data *data)
-{
-	int	i;
-	int j;
-	int	flag;
-	int start;
-	
-	start = 0;
-	flag = 0;
-	i = 0;
-	j = 0;
-	while (data->tokenizer->cmd_array[i])
-	{
-		j = 0;
-		while(data->tokenizer->cmd_array[i][j])
-		{
-			if (data->tokenizer->cmd_array[i][j] == '-')
-			{
-				j++;
-				while (data->tokenizer->cmd_array[i][j] == 'n')
-					j++;
-				if ((data->tokenizer->cmd_array[i][j] == ' ' || data->tokenizer->cmd_array[i][j] == '\0'))
-				{
-					start++;
-				}
-				else
-					break;
-			}
-			j++;
-		}
-		i++;
-	}
-	// dprintf(2, "ICI START %d\n", start);
-	return (start);
+int check_flag(t_data *data) {
+    int i;
+    int j;
+    int flag;
+    int start;
+
+    start = 0;
+    flag = 0;
+    i = 0;
+    j = 0;
+    while (data->tokenizer->cmd_array[i]) {
+        j = 0;
+        while (data->tokenizer->cmd_array[i][j]) {
+            if (data->tokenizer->cmd_array[i][j] == '-') {
+                j++;
+                // Ensure j is within bounds before further checks
+                while (data->tokenizer->cmd_array[i][j] == 'n' && data->tokenizer->cmd_array[i][j] != '\0')
+                    j++;
+                // Check if current character is a space or null terminator, and ensure it's within bounds
+                if (data->tokenizer->cmd_array[i][j] == ' ' || data->tokenizer->cmd_array[i][j] == '\0') {
+                    start++;
+                } else {
+                    break;
+                }
+            }
+            // Ensure j is incremented safely within bounds
+            if (data->tokenizer->cmd_array[i][j] != '\0')
+                j++;
+        }
+        i++;
+    }
+    // dprintf(2, "ICI START %d\n", start);
+    return (start);
 }
+
 
 int	check_echo_cmd(t_data *data)
 {
