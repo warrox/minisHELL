9ba7693456e8a93cbcf330520e66bc761060bca9
Warren Hamdi <whamdi@made-f0Ar5s4.clusters.42paris.fr>
2024-07-10 12:48:02 +0200

    [NORM] : tokenizer_util_3

--- src/tokenizer/tokenizer_util_3.c
+++ src/tokenizer/tokenizer_util_3.c
@@ -11,35 +11,37 @@
 /* ************************************************************************** */
 
 #include "../../includes/minishell_lib.h"
-int triple_sign_checker(char *str, t_data *data)
+
+int	triple_sign_checker(char *str, t_data *data)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
+
 	i = 0;
 	j = 0;
-	handle_double_quotes_flush(str, NULL, &j, 0,data,0);
+	handle_double_quotes_flush(str, NULL, &j, 0, data, 0);
 	j = 0;
-	if(data->pansement == 0)
-		handle_single_quotes_flush(str, NULL, &j, 0, data,0); 
-	if(data->pansement == 1)
-		return(-2);
-
-	while(str[i])
+	if (data->pansement == 0)
+		handle_single_quotes_flush(str, NULL, &j, 0, data, 0);
+	if (data->pansement == 1)
+		return (-2);
+	while (str[i])
 	{
-		if(ft_strncmp(&str[i], "<<<", 3)== 0 || ft_strncmp(&str[i], "<<>", 3) == 0 
-			|| ft_strncmp(&str[i], ">>>", 3) == 0 ||ft_strncmp(&str[i], ">><", 3) == 0)
+		if (ft_strncmp(&str[i], "<<<", 3) == 0 || ft_strncmp(&str[i], "<<>",
+				3) == 0 || ft_strncmp(&str[i], ">>>", 3) == 0
+			|| ft_strncmp(&str[i], ">><", 3) == 0)
 		{
 			ft_printf("minishell: syntax error near unexpected token\n");
-			return(-1);
+			return (-1);
 		}
 		i++;
 	}
-	return(0);
+	return (0);
 }
 
-void fill_array_sign(t_data *data, char *str, t_list_arg *tmp)
+void	fill_array_sign(t_data *data, char *str, t_list_arg *tmp)
 {
-	(void) str;
+	(void)str;
 	if (ft_strncmp(&tmp->input_splited[data->i], "<<", 2) == 0)
 	{
 		data->i++;
@@ -47,7 +49,7 @@ void fill_array_sign(t_data *data, char *str, t_list_arg *tmp)
 	}
 	else if (ft_strncmp(&tmp->input_splited[data->i], ">>", 2) == 0)
 	{
-		data->i++;	
+		data->i++;
 		tmp->array_sign[data->j++] = APPEND;
 	}
 	else if (ft_strncmp(&tmp->input_splited[data->i], "<", 1) == 0)
@@ -60,24 +62,23 @@ void fill_array_sign(t_data *data, char *str, t_list_arg *tmp)
 	}
 }
 
-
 int	sort_sign(t_list_arg *tmp, t_data *data)
 {
-	int flag;
+	int	flag;
+
 	flag = 0;
 	data->j = 0;
 	data->i = 0;
-
 	tmp->count_size = count_sign(tmp->input_splited);
 	tmp->array_sign = ft_calloc(tmp->count_size + 1, sizeof(int));
-	if(!tmp->array_sign)
-		return(-1);
+	if (!tmp->array_sign)
+		return (-1);
 	while (tmp->count_size && tmp->input_splited[data->i])
 	{
-		fill_array_sign(data,&tmp->input_splited[data->i], tmp);
+		fill_array_sign(data, &tmp->input_splited[data->i], tmp);
 		data->i++;
 	}
-	return(0);
+	return (0);
 }
 
 int	get_word_size(char *str)
@@ -91,5 +92,3 @@ int	get_word_size(char *str)
 		i++;
 	return (i);
 }
-
-
